5067a1bfeb773684c31c24da814ad896591c7833
Rad2007 <tiry2ooo@gmail.com>
2026-04-28 23:00:26 +0300

    повноцінна ялинка

--- results/11/practice11.asm
+++ results/11/practice11.asm
@@ -1,5 +1,6 @@
 section .data
     h           dd  9
+    trunk_h     dd  2
 
 section .bss
     line_buf    resb 60
@@ -13,7 +14,7 @@ _start:
 .row_loop:
     mov     eax, [h]
     cmp     esi, eax
-    jg      .done
+    jg      .trunk_start
 
     mov     edi, 0
 
@@ -50,6 +51,48 @@ _start:
 
     inc     esi
     jmp     .row_loop
+
+.trunk_start:
+    mov     esi, 0
+
+.trunk_loop:
+    mov     eax, [trunk_h]
+    cmp     esi, eax
+    jge     .done
+
+    mov     edi, 0
+
+    mov     ecx, [h]
+    sub     ecx, 2
+    cmp     ecx, 0
+    je      .trunk_stars
+
+.trunk_space_loop:
+    mov     byte [line_buf + edi], ' '
+    inc     edi
+    dec     ecx
+    jnz     .trunk_space_loop
+
+.trunk_stars:
+    mov     ecx, 3
+
+.trunk_star_loop:
+    mov     byte [line_buf + edi], '*'
+    inc     edi
+    dec     ecx
+    jnz     .trunk_star_loop
+
+    mov     byte [line_buf + edi], 10
+    inc     edi
+
+    push    esi
+    mov     ecx, line_buf
+    mov     edx, edi
+    call    print_line
+    pop     esi
+
+    inc     esi
+    jmp     .trunk_loop
 
 .done:
     mov     eax, 1
